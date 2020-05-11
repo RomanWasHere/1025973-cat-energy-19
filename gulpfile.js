24c7041a0ad64bfdd6fb7d0fae093e9a52c611d7
RomanWasHere <r.vlassenko@gmail.com>
2020-05-11 22:14:49 +0300

    Добавляет возможность вставлять спрайт

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 "use strict";
-
+// ------------------------------------------ //
+// 01 ПАКЕТЫ
+// ------------------------------------------ //
 var gulp = require("gulp");
 var plumber = require("gulp-plumber");
 var sourcemap = require("gulp-sourcemaps");
+var rename = require("gulp-rename");
 
 /* Пакеты для минифаикации стилей */
-var rename = require("gulp-rename");
 var sass = require("gulp-sass");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
@@ -14,8 +16,22 @@ var csso = require("gulp-csso");
 /* Пакет для оптимизации изображений */
 var imagemin = require("gulp-imagemin");
 
+/* Пакет для изображений в WebP */
+var webp = require("gulp-webp");
+
+/* Пакет для сборки SVG спрайтов */
+var svgstore = require("gulp-svgstore");
+
+/* Пакеты для вставки спрайта в HTML */
+var posthtml = require("gulp-posthtml");
+var include = require("posthtml-include");
+
 /* Пакет для локального сервера */
 var server = require("browser-sync").create();
+
+// ------------------------------------------ //
+// 02 ТАСКИ
+// ------------------------------------------ //
 
 /* Таска на обработку стилей */
 gulp.task("css", function () {
@@ -42,7 +58,33 @@ gulp.task("images", function () {
       imagemin.svgo(),
     ]))
     .pipe(gulp.dest("source/img"));
-})
+});
+
+/* Таска на конвертацию изображений в WebP */
+gulp.task("webp", function () {
+  return gulp.src("source/img/**/*.{png, jpg}")
+    .pipe(webp())
+    .pipe(gulp.dest("source/img"));
+});
+
+/* Таска на сборку SVG спрайтов */
+gulp.task("sprite", function () {
+  return gulp.src("source/img/icon-*.svg")
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("source/img"));
+});
+
+/* Таска на вставку спрайта в HTML */
+gulp.task("html", function () {
+  return gulp.src("source/*.html")
+    .pipe(posthtml({
+      include()
+    }))
+    .pipe(gulp.dest("source"));
+});
 
 /* Таска для локального сервера */
 gulp.task("server", function () {
@@ -58,4 +100,16 @@ gulp.task("server", function () {
   gulp.watch("source/*.html").on("change", server.reload);
 });
 
+
+// ------------------------------------------ //
+// 03 СЕРИИ ТАСОК
+// ------------------------------------------ //
+
 gulp.task("start", gulp.series("css", "server"));
+
+
+/* TODO:
+1 - SVG
+2 - Билд
+3 - Webp -> в отдельный <picture type="image/webp"></picture>
+*/
